5423659b80e12013169fa9ae1600894265dbab59
John LaBarge <johnlabarge@gmail.com>
2012-07-22 13:56:45 -0400

    5.6 Exercise 2 completed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,7 @@ describe "StaticPages" do
     it { should have_selector('title', text: full_title(page_title)) }
   end
 
+
   describe "Home page" do
     before { visit root_path }
     let(:heading) { 'Sample App'}
@@ -25,7 +26,7 @@ describe "StaticPages" do
   describe "About page" do
     before { visit about_path }
     let(:heading) {'About Us'}
-    let(:page_title) {'About'}
+    let(:page_title) {'About Us'}
     it_should_behave_like "all static pages"
   end
 
@@ -35,5 +36,21 @@ describe "StaticPages" do
     let(:heading) {"Contact"}
     let(:page_title) {"Contact"}
     it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text:full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text:full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text:full_title('Contact')
+    click_link "Home"
+    page.should have_selector 'title', text:full_title('')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text:full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text:full_title('')
   end
 end
