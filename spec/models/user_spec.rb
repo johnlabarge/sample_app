3c496e64b708c52e4e875fdef02dd92b4c7141e9
jlabargecb <jlabarge@collegeboard.org>
2012-08-10 16:59:59 -0400

    fix test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,7 +31,7 @@ describe User do
     let(:found_user)  {User.find_by_email(@user.email)}
 
     describe "with valid password " do
-      it {should == found_user.auenticate(@user.password)}
+      it {should == found_user.authenticate(@user.password)}
     end
     describe "with invalid password" do
       let(:user_for_invalid_password ) { found_user.authenticate("invalid")}
